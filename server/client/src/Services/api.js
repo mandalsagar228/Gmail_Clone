d395e79e7a0d11ff83bce621e636e5c9130ce6b2
Sagar Mandal <69377939+mandalsagar228@users.noreply.github.com>
2023-08-25 03:26:43 +0545

    final push for deployment

--- server/client/src/Services/api.js
+++ server/client/src/Services/api.js
@@ -1,6 +1,6 @@
 import axios from "axios";
 
-const API_URL = "http://localhost:8000";
+const API_URL = "";
 
 const API_GMAIL = async (payload, urlObject, type) => {
   return await axios({
--- server/index.js
+++ server/index.js
@@ -5,9 +5,11 @@ import router from "./Routes/Route.js";
 
 import cors from "cors";
 import bodyParser from "body-parser";
+import path from "path";
+const __dirname = path.resolve();
 
 dotenv.config();
-const PORT = 8000;
+const PORT = process.env.PORT || 8000;
 const app = express();
 app.use(cors());
 
@@ -15,6 +17,16 @@ app.use(express.json());
 app.use(bodyParser.json({ extended: true }));
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use("/", router);
+
+app.use(express.static(path.join(__dirname, "./client/build")));
+app.get("*", function (_, res) {
+  res.sendFile(
+    path.join(__dirname, "./client/build/index.html"),
+    function (error) {
+      res.status(500).send(error);
+    }
+  );
+});
 
 const USERNAME = process.env.DB_USERNAME;
 const PASSWORD = process.env.DB_PASSWORD;
